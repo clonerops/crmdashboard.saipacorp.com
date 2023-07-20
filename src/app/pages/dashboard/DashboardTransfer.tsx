e3437f9b8fc2c0f81b06f781122d92385b9316c0
iamabolfazlmasoumi <iamabolfazlmasoumi@gmail.com>
2023-07-20 09:23:58 +0330

    esale dashboard

---
+++ src/app/pages/dashboard/DashboardTransfer.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { FC } from "react";
+import { PageTitle } from "../../../_cloner/layout/core";
+import SaleTotalTypeReport from "../../modules/esale/components/SaleTotalTypeReport";
+import SaleByProductReport from "../../modules/esale/components/SaleByProductReport";
+import SaleByProductPriorityReport from "../../modules/esale/components/SaleByProductPriorityReport";
+import SaleTotalProductAndDateReport from "../../modules/esale/components/SaleTotalProductAndDateReport";
+import {
+    checkUserRole,
+    getUserRoles,
+} from "../../../_cloner/helpers/reusableFunction";
+import AccessDenied from "../../../_cloner/helpers/components/AccessDenied";
+
+const DashboardTransfer: FC = () => {
+    return (
+        <>
+            <PageTitle breadcrumbs={[]}>
+                {"داشبورد مدیریتی حمل محصول"}
+            </PageTitle>
+            {checkUserRole(
+                getUserRoles(),
+                "TransLotteryWinnerDashboardStatisticTransfer"
+            ) ? (
+                <>
+                    <div className="md:grid md:grid-cols-3 md:gap-4">
+                        <div className="mt-2 mb-2 shadow-lg">
+                            <SaleTotalTypeReport />
+                        </div>
+                        <div className="mt-2 mb-2 shadow-lg md:col-span-2">
+                            <SaleTotalProductAndDateReport />
+                        </div>
+                    </div>
+                    <div className="md:grid md:grid-cols-1 md:gap-4">
+                        <div className="mt-2 mb-2 shadow-lg">
+                            <SaleByProductReport />
+                        </div>
+                        <div className="mt-2 mb-2 shadow-lg">
+                            <SaleByProductPriorityReport />
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <AccessDenied />
+            )}
+        </>
+    );
+};
+export { DashboardTransfer };
