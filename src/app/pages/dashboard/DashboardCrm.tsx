1def1e478dc07fc893a87e98778b01f6f1cb570b
iamabolfazlmasoumi <iamabolfazlmasoumi@gmail.com>
2023-07-23 14:31:11 +0330

    'crmdashboard'

--- src/app/pages/dashboard/DashboardCrm.tsx
+++ src/app/pages/dashboard/DashboardCrm.tsx
@@ -1,17 +1,12 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { FC } from "react";
 import { PageTitle } from "../../../_cloner/layout/core";
-import SaleTotalTypeReport from "../../modules/esale/components/SaleTotalTypeReport";
-import SaleByProductReport from "../../modules/esale/components/SaleByProductReport";
-import SaleByProductPriorityReport from "../../modules/esale/components/SaleByProductPriorityReport";
-import SaleTotalProductAndDateReport from "../../modules/esale/components/SaleTotalProductAndDateReport";
 import {
     checkUserRole,
     getUserRoles,
 } from "../../../_cloner/helpers/reusableFunction";
 import AccessDenied from "../../../_cloner/helpers/components/AccessDenied";
-import BilllandingByStatus from "../../modules/transfer/components/BilllandingByStatus";
-import BilllandingCarsByStatus from "../../modules/transfer/components/BilllandingCarsByStatus";
+import ComplaintOrRequest from "../../modules/crm/components/ComplaintOrRequest";
 
 const DashboardCrm: FC = () => {
     return (
@@ -21,16 +16,12 @@ const DashboardCrm: FC = () => {
             </PageTitle>
             {checkUserRole(
                 getUserRoles(),
-                "TransLotteryWinnerDashboardStatisticCRM"
+                "TransLotteryWinnerDashboardStatistic"
             ) ? (
                 <>
                     <div className="md:grid md:grid-cols-1 md:gap-4">
-                        {/* <div className="mt-2 mb-2 shadow-lg">
-                            <BilllandingByStatus />
-                        </div> */}
-                        {/* <div className="mt-2 mb-2 shadow-lg md:col-span-2"> */}
                         <div className="mt-2 mb-2 shadow-lg">
-                            <BilllandingCarsByStatus />
+                            <ComplaintOrRequest />
                         </div>
                     </div>
                 </>
